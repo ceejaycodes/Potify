14d218e9ba75b75659d205c7261877b32f5f9732
Marvel <dreelceejay79@yahoo.com>
2023-12-12 11:22:51 +0100

    trying to fix prod issues with cart and dark/light mode

--- app/product/AddToCartButton.tsx
+++ app/product/AddToCartButton.tsx
@@ -13,23 +13,18 @@
  
  const AddToCart = ({name, id, image, unit_amount, quantity}: AddCartType) => {
     const cartStore = useCartStore()
-    const [added, setadded] = useState(false)
-
     const handleAddToCart = () => {
       cartStore.addProduct({ id, name, unit_amount, quantity, image})
-      setadded(true)
-      setTimeout(() => {
-        setadded(false)
-      }, 500)
+
     }
    return (
      <>
      <button onClick={handleAddToCart} 
-     disabled={added}
+
       className="my-4 btn btn-primary w-full"
       >
-      {!added && <span>Add To Cart</span>}
-       {added && <span> Adding To Cart 🪴</span>}
+      { <span>Add To Cart</span>}
+
         </button>
      </>
    )
